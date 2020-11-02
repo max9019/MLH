b718f2da47b82d312714a6e6c7ed087153f4277c
Maxim <maxgpod@gmail.com>
2020-11-01 18:37:23 -0800

    tc-008, tc-009

--- test/smoke/initial.js
+++ test/smoke/initial.js
@@ -39,7 +39,7 @@ describe('My Little Hero', function () {
 
     describe('Labels are correct', function () {
 
-        it('TC-006 Label for name = 1. What is your HERO\'s name?', function () {
+        it('TC-006 Label for name = 1. What is your Hero\'s name?', function () {
             const text = $$(sel.label)[0].getAttribute('title');
             expect(text).toEqual(exp.labelName);
         });
@@ -49,14 +49,14 @@ describe('My Little Hero', function () {
             expect(text).toEqual(exp.labelGender);
         });
 
-        xit('TC-008 Label for age', function () {
-            const label = $$(sel.label)[2].isDisplayed();
-            expect(label).toEqual(true);
+        it('TC-008 Label for age = 3. How old is your hero?', function () {
+            const text = $$(sel.label)[2].getAttribute('title');
+            expect(text).toEqual(exp.labelAge);
         });
 
-        xit('TC-005 Label for story', function () {
-            const label = $$(sel.label)[3].isDisplayed();
-            expect(label).toEqual(true);
+        it('TC-009 Label for story = 4. What type of story would you like to read?', function () {
+            const text = $$(sel.label)[3].getAttribute('title');
+            expect(text).toEqual(exp.labelStory);
         });
 
     });
