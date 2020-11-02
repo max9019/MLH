275311aca0abf826e9e46bd2e04bc0a8b238daa9
Maxim <maxgpod@gmail.com>
2020-11-02 01:33:34 -0800

    first functionality test

--- test/smoke/mainFunctionality.js
+++ test/smoke/mainFunctionality.js
@@ -6,8 +6,13 @@ describe('Checking the main functionality', function () {
 
         it('TC-021 Create button is clickable after 1-4 are filled in', function () {
             browser.url('https://qa-apps.netlify.app/app_my_hero');
-            const name = $(sel.name).isDisplayed();
-            expect(name).toEqual(true);
+            const inputName = $(sel.name).setValue("LadyBug");
+            const inputAge = $(sel.age).setValue(123);
+            const inputGender = $$(sel.gender)[1].click();
+            const storyClick = $(sel.storyClick).click();
+            const inputStory = $$(sel.storyType)[6].click();
+            const submit = $(sel.create).isEnabled();
+            expect(submit).toEqual(true);
         });
 
     });
